b687dbff1178160e1439af811a2208e8ca23d565
papajo <joshipv2@gmail.com>
2016-02-20 18:00:48 -0500

    Add sidenav

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,12 +3,18 @@
     
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope, classifiedsFactory) {
+        .controller("classifiedsCtrl", function($scope, classifiedsFactory, $mdSidenav) {
             
              classifiedsFactory.getClassifieds().then(function(classifieds){
                 $scope.classifieds = classifieds.data;                                      
         });
         
+        $scope.openSidebar = function() {
+            $mdSidenav('left').open();
+        }
+        $scope.closeSidebar = function() {
+            $mdSidenav('left').close();
+        }
     });
     
 })();
