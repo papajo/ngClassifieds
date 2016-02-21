f7bce87fbf4de623bd2606d3bc91d1d25fdf1701
papajo <joshipv2@gmail.com>
2016-02-20 21:55:24 -0500

    Add functionality for saving  new classifieds

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,17 +3,38 @@
     
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope, classifiedsFactory, $mdSidenav) {
+        .controller("classifiedsCtrl", function($scope, classifiedsFactory, $mdSidenav, $mdToast) {
             
              classifiedsFactory.getClassifieds().then(function(classifieds){
                 $scope.classifieds = classifieds.data;                                      
         });
+        
+        var contact = {
+            name: "Papa Jo",
+            phone: "666-555-444",
+            email: "papajo@github.com"
+        }
         
         $scope.openSidebar = function() {
             $mdSidenav('left').open();
         }
         $scope.closeSidebar = function() {
             $mdSidenav('left').close();
+        }
+        $scope.saveClassified = function(classified) {
+            if(classified) {
+                classified.contact = contact;
+                $scope.classifieds.push(classified);
+                $scope.classified = {};
+                $scope.closeSidebar();
+                $mdToast.show(
+                    $mdToast.simple()
+                      .content("Classified Saved")
+                      .position('top, right')
+                      .hideDelay(3000)
+                ); 
+            }
+            
         }
     });
     
