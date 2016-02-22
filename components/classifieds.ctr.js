c0038e08bd40231eb156627e2b99cbb0d232855a
papajo <joshipv2@gmail.com>
2016-02-22 09:31:07 -0500

    Add functionality for deleting classifieds

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
     
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope, classifiedsFactory, $mdSidenav, $mdToast) {
+        .controller("classifiedsCtrl", function($scope, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
             
              classifiedsFactory.getClassifieds().then(function(classifieds){
                 $scope.classifieds = classifieds.data;                                      
@@ -40,6 +40,19 @@
             $scope.classified = {};
             $scope.closeSidebar();
             showToast("Edit Saved");
+        }
+        $scope.deleteClassified = function(event, classified) {
+            var confirm = $mdDialog.confirm()
+                .title('Are you sure you want to delete ' + classified.title + '?')
+                .ok('Yes')
+                .cancel('No')
+                .targetEvent(event);
+            $mdDialog.show(confirm).then(function(){
+                    var index = $scope.classifieds.indexOf(classified);
+                    $scope.classifieds.splice(index, 1);
+                }, function() {
+            });
+            
         }
         function showToast(message) {
             $mdToast.show(
