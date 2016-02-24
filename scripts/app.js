17fc9781c3126ae094ae47d9bee711baaa9c1a69
papajo <joshipv2@gmail.com>
2016-02-24 16:19:42 -0500

    test basic ui-router model works

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,23 @@
 angular
-    .module("ngClassifieds", ["ngMaterial"])
-    .config(function($mdThemingProvider) {
+    .module("ngClassifieds", ["ngMaterial", "ui.router"])
+    .config(function($mdThemingProvider, $stateProvider) {
     
     $mdThemingProvider.theme('default')
         .primaryPalette('teal')
         .accentPalette('orange');
-})
-.directive("helloWorld", function() {
-    return {
-        template: "<h1>{{message}}</h1>"
-    }
+
+    $stateProvider
+        .state('one', {
+        url: '/stateone',
+        template: '<h1>State One</h1'
+    })
+        .state('two', {
+        url: '/statetwo',
+        template: '<h1>State Two</h1> <md-button ui-sref="two.more">Go to nested state</md-button><ui-view></ui-view>'
+    })
+        .state('two.more', {
+        url: '/more',
+        template: '<p>This is the deeper state</p>'
+    })
 });
 
